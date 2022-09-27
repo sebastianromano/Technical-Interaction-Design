82783de278acb81a0e1f233f7e80cdf82d16f804
Sebastian Romano <70913476+sebastianromano@users.noreply.github.com>
2022-09-27 18:23:52 +0200

    first commit

---
+++ Lectures/L5/Exercises/app_name/src/ClockDisc.js
@@ -0,0 +1,14 @@
+
+function ClockDisc() { // remember: Only one funtion in a file
+    // component functions returns jsx
+
+    return ( // css naming convention: ...
+        <div className="clock-disc">
+            <ClockHand />
+
+        </div>
+    )
+
+}
+
+export default ClockDisc;
